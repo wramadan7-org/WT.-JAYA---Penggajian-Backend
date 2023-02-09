356d308517b44cb4911d7937ad70fa56a28545ad
Wahyu Ramadan <wramadan1203@gmail.com>
2023-02-09 14:55:00 +0700

    comment service helper

--- src/services/serviceHelper.js
+++ src/services/serviceHelper.js
@@ -1,6 +1,12 @@
 const { Op } = require('sequelize');
 const { User } = require('../models');
 
+/**
+ * Service helper to find User by Email or Phone
+ * @param {String} email
+ * @param {String} phone
+ * @returns Object
+ */
 const findUserByEmailOrPhone = async (email, phone) => {
   const user = await User.findOne({
     where: {
@@ -18,6 +24,13 @@ const findUserByEmailOrPhone = async (email, phone) => {
   return user;
 };
 
+/**
+ * Service helper to find User by Email or Phone where not the own Email or Phone in ID
+ * @param {Number} id
+ * @param {String} email
+ * @param {String} phone
+ * @returns Object
+ */
 const findAnotherUserByEmailOrPhone = async (id, email, phone) => {
   const user = await User.findOne({
     where: {
