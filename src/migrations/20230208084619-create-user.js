e6197ad838c04fb81db462f5409051e72613f880
Wahyu Ramadan <wramadan1203@gmail.com>
2023-02-10 00:58:00 +0700

    typo naming password

--- src/migrations/20230208084619-create-user.js
+++ src/migrations/20230208084619-create-user.js
@@ -21,7 +21,7 @@ module.exports = {
         allowNull: false,
         unique: true,
       },
-      passwrod: {
+      password: {
         type: Sequelize.STRING,
         allowNull: false,
       },
