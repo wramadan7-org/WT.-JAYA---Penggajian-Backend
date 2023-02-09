f38025731f6d4b10cb6c9c4cd75176ba90d97817
Wahyu Ramadan <wramadan1203@gmail.com>
2023-02-09 15:01:43 +0700

    validation request when want to update karyawan

--- src/routes/v1/karyawanRoute.js
+++ src/routes/v1/karyawanRoute.js
@@ -1,14 +1,14 @@
 const { Router } = require('express');
 const karyawanController = require('../../controllers/karyawanController');
 const validation = require('../../middlewares/validator');
-const { createKaryawanValidation } = require('../../validations/karyawanValidation');
+const { createKaryawanValidation, updateKaryawanValidation } = require('../../validations/karyawanValidation');
 
 const router = Router();
 
 router.post('/', validation(createKaryawanValidation), karyawanController.karyawanCreateController);
 router.get('/', karyawanController.karyawanAllController);
 router.get('/:id', karyawanController.karyawanByIdOrRoleController);
-router.patch('/:id', karyawanController.karyawanUpdateByIdController);
+router.patch('/:id', validation(updateKaryawanValidation), karyawanController.karyawanUpdateByIdController);
 router.delete('/:id', karyawanController.karyawanDeleteByIdController);
 
 module.exports = router;
--- src/validations/karyawanValidation.js
+++ src/validations/karyawanValidation.js
@@ -1,4 +1,4 @@
-const { body } = require('express-validator');
+const { body, param } = require('express-validator');
 
 const createKaryawanValidation = [
   body('first_name')
@@ -32,6 +32,46 @@ const createKaryawanValidation = [
     .optional({ nullable: true }),
 ];
 
+const updateKaryawanValidation = [
+  param('id')
+    .isNumeric()
+    .withMessage('Parameter ID is only number'),
+  body('first_name')
+    .isString()
+    .withMessage('First name must be string')
+    .notEmpty()
+    .withMessage('First name can\'t be empty')
+    .optional(),
+  body('last_name')
+    .isString()
+    .withMessage('Last name must be string')
+    .notEmpty()
+    .withMessage('Last name can\'t be empty')
+    .optional(),
+  body('email')
+    .isEmail()
+    .notEmpty()
+    .withMessage('Email can\'t be empty')
+    .optional(),
+  body('phone')
+    .isNumeric()
+    .withMessage('Phone only contain number')
+    .isLength({ min: 10 })
+    .withMessage('Phone min 10 character')
+    .isLength({ max: 13 })
+    .withMessage('Phone max 13 character')
+    .optional(),
+  body('age')
+    .isNumeric()
+    .optional({ nullable: true }),
+  body('gender')
+    .matches(/\b(?:male|female)\b/)
+    .optional({ nullable: true }),
+  body('address')
+    .optional({ nullable: true }),
+];
+
 module.exports = {
   createKaryawanValidation,
+  updateKaryawanValidation,
 };
